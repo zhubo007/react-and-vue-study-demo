cd728ced2df6a87ca34f7227276e3913ddecff88
zhubo94941 <2812467029@qq.com>
2021-03-31 01:20:46 +0800

    typescript分支消除语法的ERROR

--- react_webpack/src/App.tsx
+++ react_webpack/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './store/index';
+import store from './store/index.js';
 import Product from "./pages/product/index";
 
 const App: React.FunctionComponent = () => {
