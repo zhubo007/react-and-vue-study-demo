608fe71c1a317e7cde1a274038e4a2aa8f72b0ce
zhubo94941 <2812467029@qq.com>
2021-04-12 23:46:26 +0800

    Hook useState useEffect方法的使用

--- react_webpack/src/utils/index.ts
+++ react_webpack/src/utils/index.ts
@@ -1,6 +1,4 @@
 import {useEffect, useState} from "react";
-import {array, number, object, string} from "prop-types";
-import {List} from "immutable";
 
 export const isFalsy = (value: any): boolean => value === 0 ? false : !value;
 
@@ -8,8 +6,10 @@ export const isFalsy = (value: any): boolean => value === 0 ? false : !value;
 export const cleanObject = (object: object) => {
     const result = {...object};
     Object.keys(result).forEach(key=>{
-        const value = object[key];
+        // @ts-ignore
+        const value = result[key];
         if (isFalsy(value)){
+            // @ts-ignore
             delete result[key]
         }
     });
@@ -36,15 +36,15 @@ export const useMount = (callback: () => void) => {
 // }
 //
 // const  log = debounce(() =>{console.log('call')},5000)
-
-export const useDebounce = <T>(value: T, delay?: number) => {
-    const {debouncedValue, setDebouncedValue} = useState(value);
+//用泛型来规范类型
+export const useDebounce = <V>(value: V, delay?: number) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
     useEffect(() => {
         //每次在value变化以后，设置一个定时器
         const timeout = setTimeout(() => setDebouncedValue(value), delay);
         //每次在上一个useEffect处理完以后再运行
-        return () => clearTimeout(timeout)
-    },(value, delay));
+        return () => clearTimeout(timeout);
+    },[value, delay]);
     return debouncedValue;
 };
 
@@ -54,7 +54,7 @@ export const useArray = <T> (initialArray: T[])=>{
     return {
         value,
         setValue,
-        add: (item: T) => setValue([...value, item])
+        add: (item: T) => setValue([...value, item]),
         clear: () => setValue([]),
         removeIndex: (index: number) => {
             const copy = [...value]
